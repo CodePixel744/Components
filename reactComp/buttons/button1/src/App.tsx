c1b74d0a27742de1124c25fb336a54655205d23e
Absoluteloser <motkulwarbalaji805@gmail.com>
2025-02-23 23:00:20 +0530

    Initial commit: Add job components library

---
+++ reactComp/buttons/button1/src/App.tsx
@@ -0,0 +1,6 @@
+import { Button1 } from './components/Button1';
+function App() {
+  return <Button1 />;
+}
+
+export default App;
---
+++ reactComp/buttons/button1/src/components/Button1.tsx
@@ -0,0 +1,11 @@
+import './Buttonstyle1.css';
+export const Button1 = () => {
+  return (
+    <div className="main">
+      <div className="wrapper">
+        <button className="btn-apply">Apply Now</button>
+        <button className="btn-save">Save Job</button>
+      </div>
+    </div>
+  );
+};
